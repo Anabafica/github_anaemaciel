74db33c5ee2c4042bb0941cd9434e23813909fb6
Anabafica <baficasilvaa@gmail.com>
2024-10-29 10:32:37 -0300

    adcição de um botao com link para cadastro

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilos.css" rel="stylesheet"> 
+
     <title>Document</title>
 </head>
 <body>
   <h1> Alunos: Ana Clara Báfica e Antonio Maciel Brito <br>
     turma: iti32 </h1> 
     <img src="img/download.jfif" />
+    <link href= "cadastros.php"> 
+    <form name="formCadastros" action="cadastros.php" method="post">
+    <input type="submit" value="Cadastrar" class="btn btn-info" name="btCadastrar"  >
 
 </body>
 </html
